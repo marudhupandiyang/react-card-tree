2d506288d511c57006946a36a544089adfb1c208
Panther <marudhupandiyang@gmail.com>
2017-11-30 16:28:50 +0530

    base package skeleton

---
+++ example/README.md
@@ -0,0 +1,7 @@
+### How to Run example
+
+ 1. git clone git@github.com:marudhupandiyang/react-card-tree.git
+ 2. cd react-card-tree/examples/
+ 3. npm install
+ 4. npm run start
+ 5. Visit http://localhost:8080 in your browser
---
+++ example/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import CardTree from "../../src";
+
+
+export const App = () => {
+  return (
+    <div className="container">
+      <h2>Simple example to demonstrate React card tree</h2>
+      <CardTree
+      />
+    </div>
+  );
+};
+
+export default App;
